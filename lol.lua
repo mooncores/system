3bb942175ce20f8349e313a4deae9e00b1868e6d
mooncores <83913929+mooncores@users.noreply.github.com>
2021-05-09 13:09:18 -0400

    Update and rename no.lua to lol.lua

---
+++ lol.lua
@@ -0,0 +1,17 @@
+local whitelisted = {"";"";"";};
+local blacklisted = {"siff";"siffori";"";};
+
+wl = false
+bl = false
+
+for i,v in pairs(whitelisted) do
+if _G.key == v then
+    wl = true
+end
+end
+
+for i,v in pairs(blacklisted) do
+if _G.key == v then
+    bl = true
+end
+end
--- no.lua
+++
@@ -1,8 +0,0 @@
-local whitelisted = {"qwe";"ewq";"wqe";};
-yes = false
-
-for i,v in pairs(whitelisted) do
-if _G.key == v then
-    yes = true
-end
-end
